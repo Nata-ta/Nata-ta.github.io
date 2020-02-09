518815b46da2f826baf431cd95111fc1f63521ef
Nataliia <natashamatvienko81094@gmail.com>
2020-02-09 20:57:54 +0200

    fixed liked buttons, added product-quantity

--- app.js
+++ app.js
@@ -10,12 +10,66 @@
 		})
 	}
 
+	// slik slider
 	$('.slick').slick({
 		dots:true,
 	})
 
 
+	// liked buttons
+	let likeButtons = document.querySelectorAll(".heart");
+	
+	for(let i = 0; i < likeButtons.length; i++) {
+		likeButtons[i].addEventListener("click", function() {
+			if(likeButtons[i].classList.contains('liked')){
+				likeButtons[i].classList.remove("liked")
+			} else {
+				likeButtons[i].classList.add("liked")
+			}
+		})
+	}
 
-	$(".btn").click(function(){
-    $(this).toggleClass('active');
-});
+
+	// product-quantity
+
+	function disabledDecrement(count,[i]) {
+		if (count<=1) {
+			decrementButtons[i].disabled = true;
+		} else {
+			decrementButtons[i].disabled = false;
+		}
+	}
+	function disabledIncrement(count,[i]) {
+		if (count>=10) {
+			incrementButtons[i].disabled = true;
+		} else {
+			incrementButtons[i].disabled = false;
+		}
+	}
+
+	let decrementButtons = document.querySelectorAll(".decrement-button");
+	let incrementButtons = document.querySelectorAll(".increment-button");
+	let productQuantityInput = document.querySelectorAll(".product-quantity-input");
+	
+	
+	for(let i = 0; i < incrementButtons.length; i++) {
+	incrementButtons[i].addEventListener("click", function(){
+		let currentValue = +productQuantityInput[i].value;
+		let nextValue = currentValue + 1;
+		productQuantityInput[i].value = nextValue;
+		
+		disabledDecrement(nextValue,[i]);
+		disabledIncrement(nextValue,[i]);
+	})
+	}
+
+	for(let i = 0; i < decrementButtons.length; i++) {
+	decrementButtons[i].addEventListener("click", function(){
+		let currentValue = +productQuantityInput[i].value;
+		let nextValue = currentValue - 1;
+		productQuantityInput[i].value = nextValue;
+
+		disabledDecrement(nextValue,[i]);
+		disabledIncrement(nextValue,[i]);
+	})
+	}
